4d21fb4f025375564ae068d3cb0ce4381ee637eb
winston augustus chung <18.chung.winston@ctkprep.org>
2019-07-12 00:15:10 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -15,8 +15,7 @@ def hopper
           :languages => ["C"]
         }
      }
-
-
+  programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
@@ -37,6 +36,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+    programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +55,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+    programmer_hash[:dennis_ritchie][:languages]="C"
 end
 
 def adding_matz
@@ -66,6 +67,10 @@ def adding_matz
 
 	programmer_hash = 
  		{
+ 		    :yukihiro_matsumoto => {
+          :known_for => "Ruby",
+          :languages => ["LISP", "C"]
+ 		    },
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
@@ -78,7 +83,7 @@ def adding_matz
           :known_for => "Unix",
           :languages => ["C"]
         }
-     }
+ 		}
 
     
 end
@@ -99,9 +104,9 @@ def changing_alan
           :languages => ["C"]
         }
      }
-     #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
-     
+     programmer_hash[:alan_kay][:known_for]=alans_new_info
+     programmer_hash
      
 end
 
@@ -121,5 +126,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+    programmer_hash[:dennis_ritchie][:languages].push("Assembly")
+    programmer_hash
 end
