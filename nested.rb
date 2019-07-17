f5bb2a01f63b91155e06f7871f7c602c9a363442
Anthony Prescott-Brooks <anthonybrooks226@gmail.com>
2019-07-17 04:32:07 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -84,7 +84,11 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
+     
+     programmer_hash[:yukihiro_matsumoto] = {}
+     programmer_hash[:yukihiro_matsumoto][:known_for] = 'Ruby'
+     programmer_hash[:yukihiro_matsumoto][:languages] = ['LISP', 'C']
+     programmer_hash
     
 end
 
@@ -107,6 +111,8 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      
+     programmer_hash[:alan_kay][:known_for] = alans_new_info
+     programmer_hash
      
 end
 
@@ -126,5 +132,8 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
+     
+     programmer_hash[:dennis_ritchie][:languages][1] = 'Assembly'
+     programmer_hash
 
 end
