a9db516f77c01591b37aa2b0a0f5e702e7ec40b8
Joey Luis Echevarria <jechevarria719@gmail.com>
2019-08-04 16:14:01 +0000

    Automatically backed up by Learn

--- spec/nested_spec.rb
+++ spec/nested_spec.rb
@@ -4,14 +4,14 @@ require_relative "../nested.rb"
 describe "nested hash" do
   describe "hopper" do
     it "operates on the programmer_hash and returns the value of the :grace_hopper key" do
-      expect(hopper).to eq({:known_for => "COBOL", :languages => ["COBOL", "FORTRAN"]})
+      expect(hopper).to eq({":known_for => COBOL, :languages => ['COBOL', 'FORTRAN']})
 
     end
   end
 
   describe "alan_kay_is_known_for" do
-    it "operates on the programmer_hash and returns the value of what Alan Kay is known for" do
-      expect(alan_kay_is_known_for).to eq("Object Orientation")
+    it "operates on the programmer_hash and returns the value of what 'Alan_Kay' is known for" do
+      expect(alan_kay_is_known_for).to eq('Object Orientation')
     end
   end
 
