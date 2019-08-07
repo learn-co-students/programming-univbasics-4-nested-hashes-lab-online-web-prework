a6fc0c78a4b770ffa7dde85d3bee40b4bc82a727
Jessica Cervantes <cervantesjess1994@gmail.com>
2019-08-07 08:40:14 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -1,6 +1,6 @@
 
 def hopper
-	programmer_hash = 
+	programmer_hash =
  		{
         :grace_hopper => {
           :known_for => "COBOL",
@@ -16,13 +16,15 @@ def hopper
         }
      }
 
+grace_hopper[1]
 
 end
 
 def alan_kay_is_known_for
-	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
-	
-	programmer_hash = 
+	alan_kay[:known_for]
+
+
+	programmer_hash =
  		{
         :grace_hopper => {
           :known_for => "COBOL",
@@ -40,7 +42,7 @@ def alan_kay_is_known_for
 end
 
 def dennis_ritchies_language
-	programmer_hash = 
+	programmer_hash =
  		{
         :grace_hopper => {
           :known_for => "COBOL",
@@ -55,16 +57,17 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+		 dennis_ritchie[1].string
+
 end
 
-def adding_matz
-# add the following information to the top level of programmer_hash
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#    :languages => ["LISP", "C"]
-# }
 
-	programmer_hash = 
+def adding_matz
+	hash.new = {:yukihiro_matsumoto}
+	yukihiro_matsumoto[:known_for => "Ruby", :languages => {}]
+	:languages["LISP", "C"]
+
+	programmer_hash =
  		{
         :grace_hopper => {
           :known_for => "COBOL",
@@ -80,11 +83,11 @@ def adding_matz
         }
      }
 
-    
+
 end
 
 def changing_alan
-	programmer_hash = 
+	programmer_hash =
  		{
         :grace_hopper => {
           :known_for => "COBOL",
@@ -99,14 +102,14 @@ def changing_alan
           :languages => ["C"]
         }
      }
-     #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
+
+		 alan_kay[:known_for] = "GUI"
      alans_new_info = "GUI"
-     
-     
+
 end
 
 def adding_to_dennis
-	programmer_hash = 
+	programmer_hash =
  		{
         :grace_hopper => {
           :known_for => "COBOL",
@@ -121,5 +124,5 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+		 dennis_ritchie[:languages] = "Assembly"
 end
