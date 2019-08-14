319bb6f04cd0c9c5a17aefec0ed0293b88c72583
munificent-commit-7390 <jaredjames2020@gmail.com>
2019-08-14 00:19:04 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -1,6 +1,6 @@
 
 def hopper
-	programmer_hash = 
+	programmer_hash =
  		{
         :grace_hopper => {
           :known_for => "COBOL",
@@ -15,8 +15,9 @@ def hopper
           :languages => ["C"]
         }
      }
-
-
+  
+  p programmer_hash[:grace_hopper]
+  
 end
 
 def alan_kay_is_known_for
@@ -37,6 +38,9 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+  
+  p programmer_hash[:alan_kay][:known_for]
+     
 end
 
 def dennis_ritchies_language
@@ -55,6 +59,10 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+  
+  programmer_hash[:dennis_ritchie][:languages] = "C"   
+  p programmer_hash[:dennis_ritchie][:languages]
+  
 end
 
 def adding_matz
@@ -80,6 +88,8 @@ def adding_matz
         }
      }
 
+programmer_hash = {:yukihiro_matsumoto => {:known_for => "Ruby"}}
+programmer_hash = {:yukihiro_matsumoto => {:languages => ["LISP", "C"]}}
     
 end
 
@@ -102,6 +112,8 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      
+  programmer_hash[:alan_kay][:known_for] = "GUI"
+  p programmer_hash
      
 end
 
@@ -116,10 +128,13 @@ def adding_to_dennis
           :known_for => "Object Orientation",
           :languages => ["Smalltalk", "LISP"]
         },
-        :dennis_ritchie => {
+          :dennis_ritchie => {
           :known_for => "Unix",
           :languages => ["C"]
         }
      }
+
+  programmer_hash[:dennis_ritchie][:languages] << ("Assembly")
+  programmer_hash
 
 end
