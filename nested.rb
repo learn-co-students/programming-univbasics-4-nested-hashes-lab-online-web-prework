b062c85916832ff2edae5c8a8fa0e34ea21dc05d
Cody Dupuis <ctdupuis7@gmail.com>
2019-08-23 23:50:31 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -15,7 +15,7 @@ def hopper
           :languages => ["C"]
         }
      }
-
+programmer_hash[:grace_hopper]
 
 end
 
@@ -37,7 +37,9 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     programmer_hash[:alan_kay][:known_for]
 end
+
 
 def dennis_ritchies_language
 	programmer_hash = 
@@ -55,7 +57,11 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+    if programmer_hash[:dennis_ritchie][:languages]
+      return "C"
+    end
 end
+
 
 def adding_matz
 # add the following information to the top level of programmer_hash
@@ -79,8 +85,8 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
-    
+  programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]}
+    return programmer_hash
 end
 
 def changing_alan
@@ -101,6 +107,8 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
+     programmer_hash[:alan_kay][:known_for] = "GUI"
+     return programmer_hash
      
      
 end
@@ -121,5 +129,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+programmer_hash[:dennis_ritchie][:languages] = ["C", "Assembly"]
+return programmer_hash
 end
