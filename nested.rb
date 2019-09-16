f8abef0d629c7d70c8c6def60997d941bc4a0c9e
Firooz Mafakaheri <firooz.mafakheri@gmail.com>
2019-09-16 20:11:58 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -15,7 +15,7 @@ def hopper
           :languages => ["C"]
         }
      }
-
+   return   programmer_hash[:grace_hopper]
 
 end
 
@@ -37,6 +37,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+    return programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +56,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     return dennis_ritchies_language[:dennis_ritchie][:languages]
 end
 
 def adding_matz
@@ -65,7 +67,10 @@ def adding_matz
 # }
 
 	programmer_hash = 
- 		{
+ 		{:yukihiro_matsumoto => {
+  :known_for => "Ruby",
+   :languages => ["LISP", "C"]
+ }
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
@@ -102,7 +107,7 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      
-     
+     programmer_hash[:alan_kay][:known_for]="GUI"
 end
 
 def adding_to_dennis
@@ -121,5 +126,5 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+adding_to_dennis[:dennis_ritchie][:languages][1]="Assembly"
 end
