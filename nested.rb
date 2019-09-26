c7513e7809012d322de91474ab4a6ad9acf29b18
Laura Hatch <laura.hatch.lh@gmail.com>
2019-09-26 17:21:48 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -1,9 +1,10 @@
 
 def hopper
-	programmer_hash = {:grace_hopper => {
-		:known_for => "COBOL",
-		:languages => ["COBOL", "FORTRAN"]
-        },
+	programmer_hash =
+	{
+		:grace_hopper => {
+			:known_for => "COBOL",
+			:languages => ["COBOL", "FORTRAN"]
 			},
         :alan_kay => {
           :known_for => "Object Orientation",
@@ -13,8 +14,9 @@ def hopper
           :known_for => "Unix",
           :languages => ["C"]
         }
+			}
 
-
+programmer_hash[:grace_hopper]
 
 end
 
@@ -34,8 +36,10 @@ def alan_kay_is_known_for
         :dennis_ritchie => {
           :known_for => "Unix",
           :languages => ["C"]
-        }
+        },
      }
+
+		 programmer_hash[:alan_kay]["known_for"] = "Object Orientation"
 end
 
 def dennis_ritchies_language
@@ -51,20 +55,22 @@ def dennis_ritchies_language
         },
         :dennis_ritchie => {
           :known_for => "Unix",
-          :languages => ["C"]
+          :languages => "C"
         }
+
      }
+		 programmer_hash[:dennis_ritchie][:languages] = "C"
 end
 
 def adding_matz
-# add the following information to the top level of programmer_hash
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#    :languages => ["LISP", "C"]
-# }
+
 
 	programmer_hash =
  		{
+			:yukihiro_matsumoto => {
+			   :known_for => "Ruby",
+			   :languages => ["LISP", "C"]
+			 },
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
@@ -99,7 +105,6 @@ def changing_alan
         }
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
-     alans_new_info = "GUI"
 
 
 end
