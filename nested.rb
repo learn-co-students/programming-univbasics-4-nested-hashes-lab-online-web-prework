239971216f8282227534fa9401fca41946c4ec43
Tyler Carroll <tylercarroll1611@gmail.com>
2019-09-30 23:50:11 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -16,6 +16,7 @@ def hopper
         }
      }
 
+programmer_hash[:grace_hopper]
 
 end
 
@@ -37,9 +38,11 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
+  
 	programmer_hash = 
  		{
         :grace_hopper => {
@@ -55,7 +58,9 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-end
+     
+    programmer_hash[:dennis_ritchie][:languages][0]
+ end
 
 def adding_matz
 # add the following information to the top level of programmer_hash
@@ -80,7 +85,13 @@ def adding_matz
         }
      }
 
-    
+     programmer_hash[:yukihiro_matsumoto] = {
+  :known_for => "Ruby", 
+  :languages => ["LISP", "C"] 
+}
+
+programmer_hash
+
 end
 
 def changing_alan
@@ -102,6 +113,9 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      
+     programmer_hash[:alan_kay][:known_for] = alans_new_info = "GUI"
+     
+     programmer_hash
      
 end
 
@@ -122,4 +136,6 @@ def adding_to_dennis
         }
      }
 
+programmer_hash[:dennis_ritchie][:languages][1] = "Assembly"
+programmer_hash
 end
