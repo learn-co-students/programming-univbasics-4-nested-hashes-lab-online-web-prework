f90a0377221ab47132d1282e7ce24752c367b57f
William <teststuffreally@gmail.com>
2019-10-14 16:41:17 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -66,11 +66,8 @@ def adding_matz
 #   :known_for => "Ruby",
 #    :languages => ["LISP", "C"]
 # }
-              programmer_hash ={ 
-              :yukihiro_matsumoto => {}
-              :known_for => "Ruby",
-              :languages => "LISP", "C"
-              }
+           
+              programmer_hash=
  		{ 
  		  	  :grace_hopper => {
           :known_for => "COBOL",
