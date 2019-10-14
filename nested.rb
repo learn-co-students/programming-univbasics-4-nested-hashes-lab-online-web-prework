22cd5677377bc5e2b3775dc926d24eb4a72d5b85
William <teststuffreally@gmail.com>
2019-10-14 18:57:55 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -66,15 +66,10 @@ def adding_matz
 #   :known_for => "Ruby",
 #    :languages => ["LISP", "C"]
 # }
-     {       
-              programmer_hash={
-                :known_for => [Ruby],
-                :languages => [LISP],
-                :languages => [C],
-              
-         },
-     }
- 		  	  :grace_hopper => {
+            
+     	programmer_hash = 
+ 		{
+        :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
         },
@@ -85,10 +80,10 @@ def adding_matz
         :dennis_ritchie => {
           :known_for => "Unix",
           :languages => ["C"]
-          
         }
-     }
-     programmer_hash[:grace_hopper][:alan_kay][:dennis_ritchie]
+     }         
+     programmer_hash[:yukihiro_matsumoto]={}
+     return programmer_hash
 end
 
 def changing_alan
@@ -108,8 +103,9 @@ def changing_alan
         }
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
-     alans_new_info = "GUI"
-     
+    
+     programmer_hash[:alan_kay][:known_for]="GUI"
+     return programmer_hash
 end
 
 def adding_to_dennis
@@ -127,7 +123,9 @@ def adding_to_dennis
           :known_for => "Unix",
           :languages => ["C"]
           
+          
      }
  	}   
-
+programmer_hash[:dennis_ritchie][:languages]<<"Assembly"
+return programmer_hash
 end
