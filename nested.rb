e3db3dd4495f3ee3f5da578e5780fca0ccca0a72
Nikelson Exantus <nikelsone1981@hotmail.com>
2019-11-01 03:42:34 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -68,7 +68,8 @@ def adding_matz
         
      }
 
-    programmer_hash[:yukihiro_matsumoto, [:known_for] = "Ruby", [:languages] = "COBOL", "FORTRAN"]
+    programmer_hash[:yukihiro_matsumoto]={}
+    programmer_hash
 end
 
 def changing_alan
@@ -88,9 +89,10 @@ def changing_alan
         }
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
-     alans_new_info = "GUI"
-     alan_kay["known_for", "alans_new_info"]
-     
+     #alans_new_info = "GUI"
+    programmer_hash[:alan_kay] = {}
+     programmer_hash[:alan_kay][:known_for] = "GUI"
+     programmer_hash
 end
 
 def adding_to_dennis
@@ -109,5 +111,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+programmer_hash[:dennis_ritchie][:languages].push("Assembly") 
+programmer_hash
 end
